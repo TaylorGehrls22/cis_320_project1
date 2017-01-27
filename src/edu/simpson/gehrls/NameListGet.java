5eab124baf738d7f3741ba8eb135cee8f8ed5f55
Taylor <taylor.gehrls@my.simpson.edu>
2017-01-27 15:14:25 -0600

    Database Connection

---
+++ src/edu/simpson/gehrls/NameListGet.java
@@ -0,0 +1,72 @@
+package edu.simpson.gehrls;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+/**
+ * Created by taylor.gehrls on 1/26/2017.
+ */
+public class NameListGet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        response.setContentType("application/json");
+        PrintWriter out = response.getWriter();
+
+        // Use the DAO to get a list of people
+        List <Person> peopleList = PersonDAO.getPeople();
+
+        // Start the JSON output. We'll have an array of people, so start with a "[" which signifies an array
+        out.print("[");
+
+        // Keep a boolean so we can figure out where to put the stupid commas.
+        boolean start = false;
+
+        // Loop through each person in the list.
+        for (Person person : peopleList) {
+
+            // Here we need to figure out if we should print a comma
+            if (!start) {
+                start = true;
+                out.println();
+            } else
+                out.println(",");
+
+            // Start printing out field, name combinations.
+            out.print("{\"id\":");
+            out.print("\"");
+            out.print(person.getId());
+            out.print("\",");
+
+            out.print("\"firstName\":");
+            out.print("\"");
+            out.print(person.getFirst());
+            out.print("\",");
+
+            out.print("\"lastName\":");
+            out.print("\"");
+            out.print(person.getLast());
+            out.print("\",");
+
+            out.print("\"phone\":");
+            out.print("\"");
+            out.print(person.getPhone());
+            out.print("\",");
+
+            out.print("\"birthday\":");
+            out.print("\"");
+            out.print(person.getBirthday());
+            out.print("\"");
+
+            out.print("}");
+        }
+        out.println("\r\n]");
+    }
+}
---
+++ src/edu/simpson/gehrls/PersonDAO.java
@@ -0,0 +1,73 @@
+package edu.simpson.gehrls;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Created by taylor.gehrls on 1/26/2017.
+ */
+public class PersonDAO {
+    public static List<Person> getPeople() {
+        final Logger log = Logger.getLogger(PersonDAO.class.getName());
+
+        log.log(Level.FINE, "Get people");
+
+        // Create an empty linked list to put the people we get from the database into.
+        List<Person> list = new LinkedList<Person>();
+
+        // Declare our variables
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+
+        // Databases are unreliable. Use some exception handling
+        try {
+            // Get our database connection
+            conn = DBHelper.getConnection();
+
+            // This is a string that is our SQL query.
+            String sql = "select id, first, last, phone, birthday from person";
+
+            // Create an object with all the info about our SQL statement to run.
+            stmt = conn.prepareStatement(sql);
+
+            // Execute the SQL and get the results
+            rs = stmt.executeQuery();
+
+            // Loop through each record
+            while(rs.next()) {
+
+                Person person = new Person();
+
+                // Get the data from the result set, and copy it to the Person object
+                person.setId(rs.getInt("id"));
+                person.setFirst(rs.getString("first"));
+                person.setLast(rs.getString("last"));
+                person.setPhone(rs.getString("phone"));
+                person.setBirthday(rs.getString("birthday"));
+
+
+                // Add this person to the list so we can return it.
+                list.add(person);
+
+            }
+        } catch (SQLException se) {
+            log.log(Level.SEVERE, "SQL Error", se );
+        } catch (Exception e) {
+            log.log(Level.SEVERE, "Error", e );
+        } finally {
+            // Ok, close our result set, statement, and connection
+            try { rs.close(); } catch (Exception e) { log.log(Level.SEVERE, "Error", e ); }
+            try { stmt.close(); } catch (Exception e) { log.log(Level.SEVERE, "Error", e ); }
+            try { conn.close(); } catch (Exception e) { log.log(Level.SEVERE, "Error", e ); }
+        }
+        // Done! Return the results
+        return list;
+    }
+}
